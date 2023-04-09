e6de35feafe3558ab50384695cc865a3ae1f2c9e
Illucious <visheshgarg570@gmail.com>
2023-04-09 17:08:41 +0530

    added dynamic views and urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+"""urlpatterns = [
     path('', views.challenges_main),
     path('january', views.january_challenge),
     path('february', views.february_challenge),
-
+]
+"""
+urlpatterns = [
+    path('', views.challenges_main),
+    path('<month>', views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
 def challenges_main(request):
     return HttpResponse("This is the main page for challenges.")
 
-
+"""
 def january_challenge(request):
     return HttpResponse("This is the response for the january challenge.")
 
 
 def february_challenge(request):
     return HttpResponse("This is the response for the february challenge.")
+"""
+#dynamic view
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "This is the response for the january challenge."
+    elif month == "february":
+        challenge_text = "This is the response for the february challenge."
+    elif month == "march":
+        challenge_text = "This is the response for the march challenge."
+    else:
+        return HttpResponseNotFound("This month is not supported.")
+    return HttpResponse(challenge_text)
